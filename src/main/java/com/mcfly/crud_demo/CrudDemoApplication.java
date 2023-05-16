e7bff8f36d80815f00c5aac14541af95218cef03
polnikov.andrey <polnikov.andrey1981@gmail.com>
2023-05-16 10:03:30 +0300

    + CommandLineRunner bean + jdbc properties + reduced root logging level + turned of the Spring Boot banner

--- src/main/java/com/mcfly/crud_demo/CrudDemoApplication.java
+++ src/main/java/com/mcfly/crud_demo/CrudDemoApplication.java
@@ -1,7 +1,9 @@
 package com.mcfly.crud_demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudDemoApplication {
@@ -10,4 +12,11 @@ public class CrudDemoApplication {
 		SpringApplication.run(CrudDemoApplication.class, args);
 	}
 
+	// CommandLineRunner is executed after the Spring Beans have been loaded.
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello, world!");
+		};
+	}
 }
